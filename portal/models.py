1399f40a10b47e2c629e06693335736de7f6d281
shah-crypto <prathamshah2507@gmail.com>
2021-08-28 21:27:03 +0530

    update models.py

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Test(models.Model):
+    test_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, blank=True)
+    link = models.URLField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    marks_available = models.PositiveIntegerField(default=20)
+
+    def __str__(self):
+        return self.test_name
+
+class Assignment(models.Model):
+    assignment_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, blank=True)
+    link = models.URLField()
+    upload_time = models.DateTimeField(default=datetime.now)
+    deadline = models.DateTimeField()
+    marks_available = models.PositiveIntegerField(default=20)
+
+    def __str__(self):
+        return self.assignment_name
