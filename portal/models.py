3442fbbbbc2a1fe3d6e97a4002053e822ceb97c9
Burhank13 <burhan.khambati13@gmail.com>
2021-08-30 22:20:42 +0530

    Added Login Authentication

--- portal/models.py
+++ portal/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from datetime import datetime
 from django import forms
-
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255,unique=True)
     password = models.CharField(max_length=100)
     profession = models.CharField(max_length=15)
-
     class Meta:
         verbose_name_plural = "people"
     
--- portal/views.py
+++ portal/views.py
@@ -1,4 +1,5 @@
 from django.http.response import HttpResponseRedirect
+from django.contrib import messages
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
@@ -12,6 +13,7 @@ def home(request):
 
 def signup(request):
     if request.method == "POST":
+        print("signup")
         profession = request.POST.get('profession')
     return render(request, 'signup.html',{'profession':profession})
 
@@ -19,13 +21,28 @@ def choice(request):
     return render(request, 'choice.html')
 
 def login(request):
+    print("strign func")
     if request.method == "POST":
         email = request.POST.get('email')
         password =  request.POST.get('password')
-        profession = request.POST.get('profession') 
+        profession = request.POST.get('profession')
+        user=Person.objects.get(email=email)
+        if user is not None:
+            if user.password==password and user.profession==profession:
+                print("strign")
+                messages.info(request,'Logged in')
+                return redirect('/') 
+            else:
+                print("strignsss")
+                messages.info(request,'invalid credentials')
+                return redirect('/')
+        else:
+            print("strign esle")
+        
     return redirect('/')    
     
 def register(request):
+    print("register")
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
